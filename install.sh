d05d7d4b47ab829ec60e9ae7fdb851c1d9aeb8fb
John Vishnefske <jvishnefske@gmail.com>
2017-06-21 08:55:18 -0400

    added network share, synergy, kernel config

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ function safe_link(){
 safe_link .profile
 safe_link .bashrc
 safe_link .vimrc
+safe_link .gitconfig
 # if vim is installed, then install vim-plug
 if $(which vim > /dev/null)
 then
